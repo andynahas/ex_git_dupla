2c6f00b16c6b493941319a2e6d6e32303ba9f0ab
andynahas <andre.barbosa53@etec.sp.gov.br>
2019-09-30 21:59:03 -0300

    aluno A 1º commit

---
+++ ex_git_dupla/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex_git_dupla
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0, y;
+            for (x = 1; x <= 100; x++)
+            {
+                if (x % 10 == 0)
+                {
+                    Console.WriteLine(x + " MÚLTIPLO DE 10");
+                }
+                else
+                {
+                    Console.WriteLine(x);
+                }
+            }
+            Console.ReadKey();
+        }
+
+    }
+}
